0e3a3e7041b5491bf4ab37a8aac140f1c73b31b3
André Lucas <andre.lucas@storageos.com>
2017-09-26 10:12:44 +0100

    Remove known-bad binary download

--- provision.sh
+++ provision.sh
@@ -111,7 +111,8 @@ function download_storageos_cli()
 
     echo "Testing downloaded CLI using Docker"
     if ! docker run -v "$(pwd)":/mnt ubuntu:xenial /mnt/"$cli_binary" --help >/dev/null; then
-      echo "Failed to run storageos binary '$cli_binary'" >&2
+      echo "Failed to run storageos binary '$cli_binary', removing it" >&2
+      rm $cli_binary
       exit 1
     fi
 
